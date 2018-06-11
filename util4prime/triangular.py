3ce81edec47d7f0fdf30dbbdcb91a4aaac66e473
PandaLz <roayfx000@gmail.com>
2018-06-11 14:08:48 -0500

    added to util4prime module that handles triangular numbers and added triangular number generation section to index

---
+++ util4prime/triangular.py
@@ -0,0 +1,8 @@
+#module intended to do operations related to triangular number and derivatives generation
+
+def triangular(num: int) -> int:
+
+    return int((num * (num + 1) / 2))
+
+if __name__ == "__main__":
+    main()
