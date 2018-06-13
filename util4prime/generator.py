80901425d2b2f9e18a4f4ac85a4b64c57c03c13f
PandaLz <roayfx000@gmail.com>
2018-06-13 17:32:28 -0500

    continuing docment to add the generation sequences, first step for generating the first sequence done

--- util4prime/generator.py
+++ util4prime/generator.py
@@ -1,9 +1,32 @@
 def generator_backwards(num: int):
 
+    sequence = []
+    control_sequence = []
+
+    current = num;
+    top = num;
+    bottom = num - 1
+
+    sequence.append([bottom])
+
+
     if num % 2 == 0:
 
-        pass
+        current = current + 1
+
+        control_sequence.append(current)
+
+        print(sequence)
+        print(control_sequence)
+
+        
 
     else:
 
-        pass
+        control_sequence.append(current)
+
+        print(sequence)
+        print(control_sequence)
+
+
+
